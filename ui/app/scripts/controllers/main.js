fad904d2d32c00df9172de46460d7af030d56fad
JTB <jtbdevelopment@gmail.com>
2015-12-16 22:25:47 -0500

    simple chase logic code cleanups

--- ui/app/scripts/controllers/main.js
+++ ui/app/scripts/controllers/main.js
@@ -22,11 +22,27 @@ angular.module('uiApp')
         };
 
         gameStates.TestMaze = function () {
+            this.PLAYER_MOVE_SPEED = 75;
+            this.PLAYER_MASS = 10;
+
+            this.ROCK_MASS = 80;
+
+            this.DEMON_PATROL_SPEED = 25;
+            this.DEMON_CHASE_SPEED = 85;
+            this.DEMON_PATROL_RANGE = 64;
+            this.DEMON_MAX_SIGHT = 100;
+
+            this.LIGHT_RADIUS = 100;
+
+            this.DEBUG = false;
+
             this.player = null;
             this.cursors = null;
             this.blockLayer = null;
             this.enemyGroup = null;
             this.rockGroup = null;
+
+            this.tileHits = [];
         };
         gameStates.TestMaze.prototype = {
             //  TODO - contemplate lighting like http://www.html5gamedevs.com/topic/3052-phaser-and-2d-lighting/
@@ -59,14 +75,15 @@ angular.module('uiApp')
                 //  http://phaser.io/examples/v2/p2-physics/load-polygon-1
 
                 this.player = this.game.add.sprite(16, 1264, 'player');
+                this.game.physics.p2.enable(this.player);
+                this.player.body.collideWorldBounds = true;
+                this.player.body.fixedRotation = true;
+                this.player.body.debug = this.DEBUG;
+                //  TODO - size with real image
                 this.player.height = 32;
                 this.player.width = 32;
-                this.player.anchor.set(0.5);
-                this.game.physics.p2.enable(this.player);
                 this.player.body.setCircle(10);
-                this.player.body.collideWorldBounds = true;
-                this.player.body.mass = 10;
-                this.player.body.fixedRotation = true;
+                this.player.body.mass = this.PLAYER_MASS;
 
                 var playerMaterial = game.physics.p2.createMaterial('playerMaterial', this.player.body);
                 var worldMaterial = game.physics.p2.createMaterial('worldMaterial');
@@ -106,41 +123,43 @@ angular.module('uiApp')
                     frictionRelaxation: 3,
                     surfaceVelocity: 0
                 });
-                this.game.camera.follow(this.player);
 
-                this.game.physics.p2.createCollisionGroup();
                 this.enemyGroup = this.game.add.physicsGroup(Phaser.Physics.P2JS);
 
                 this.rockGroup = this.game.add.physicsGroup(Phaser.Physics.P2JS);
 
                 map.createFromObjects('Object Layer', 214, 'hyptosis_tile-art-batch-1', 214, true, false, this.rockGroup);
-                angular.forEach(this.rockGroup.children, function (rock) {
+                this.rockGroup.forEach(function (rock) {
+                    rock.body.setMaterial(rockMaterial);
+                    rock.body.collideWorldBounds = true;
+                    rock.body.mass = this.ROCK_MASS;
+                    rock.body.damping = 0.95;
+                    rock.body.angularDamping = 0.85;
+                    rock.body.debug = this.DEBUG;
+
+                    //  TODO - size with real image
                     rock.height = 40;
                     rock.width = 40;
                     rock.body.setRectangle(40, 40, 0, 0);
-                    rock.body.collideWorldBounds = true;
-                    rock.body.mass = 80;
-                    rock.body.damping = 0.95;
-                    rock.body.angularDamping = 0.85;
-                    rock.body.setMaterial(rockMaterial);
                 }, this);
 
                 map.createFromObjects('Object Layer', 782, 'demon', 0, true, false, this.enemyGroup);
-                angular.forEach(this.enemyGroup.children, function (enemy) {
+                this.enemyGroup.forEach(function (enemy) {
                     enemy.height = 32;
                     enemy.width = 32;
+                    enemy.body.setCircle(11);
                     enemy.initialX = enemy.x;
                     enemy.initialY = enemy.y;
-                    enemy.minX = enemy.initialX - 64;
-                    enemy.maxX = enemy.initialX + 64;
-                    enemy.minY = enemy.initialY - 64;
-                    enemy.maxY = enemy.initialY + 64;
+                    enemy.minX = enemy.initialX - this.DEMON_PATROL_RANGE;
+                    enemy.maxX = enemy.initialX + this.DEMON_PATROL_RANGE;
+                    enemy.minY = enemy.initialY - this.DEMON_PATROL_RANGE;
+                    enemy.maxY = enemy.initialY + this.DEMON_PATROL_RANGE;
                     enemy.isChasing = false;
-                    enemy.body.setCircle(11);
+                    enemy.body.debug = this.DEBUG;
                     enemy.body.collideWorldBounds = true;
                     enemy.body.fixedRotation = true;
-                    enemy.body.velocity.x = 25;
-                    enemy.body.velocity.y = 25;
+                    enemy.body.velocity.x = this.DEMON_PATROL_SPEED;
+                    enemy.body.velocity.y = this.DEMON_PATROL_SPEED;
                     enemy.body.setZeroDamping();
                     enemy.body.setMaterial(demonMaterial);
                 }, this);
@@ -151,13 +170,44 @@ angular.module('uiApp')
                 this.shadowTexture = this.game.add.bitmapData(this.game.world.width, this.game.world.height);
                 this.lightSprite = this.game.add.image(this.game.camera.x, this.game.camera.y, this.shadowTexture);
                 this.lightSprite.blendMode = Phaser.blendModes.MULTIPLY;
+
+                this.game.camera.follow(this.player);
             },
 
             update: function () {
                 this.player.body.setZeroVelocity();
-                angular.forEach(this.enemyGroup.children, function (enemy) {
+
+                this.enemyGroup.forEach(function (enemy) {
+                    //  TODO - Play sound while chasing or play sound when chase begins?
+                    var ray = new Phaser.Line(enemy.x, enemy.y, this.player.x, this.player.y);
+                    enemy.isChasing = false;
+                    if (ray.length < this.DEMON_MAX_SIGHT) {
+                        if (this.DEBUG) {
+                            angular.forEach(this.tileHits, function (tileHit) {
+                                tileHit.debug = false;
+                            });
+                            this.blockLayer.dirty = this.tileHits.length > 0;
+                        }
+                        this.tileHits = this.blockLayer.getRayCastTiles(ray, undefined, true);
+                        if (this.DEBUG) {
+                            angular.forEach(this.tileHits, function (tileHit) {
+                                tileHit.debug = this.DEBUG;
+                            }, this);
+                            this.blockLayer.dirty = this.tileHits.length > 0;
+                        }
+
+                        //  TODO - this rock test code does not work
+                        var rocksHit = [];
+                        var lineCoordinates = ray.coordinatesOnLine(1);
+                        angular.forEach(lineCoordinates, function (point) {
+                            rocksHit.push(this.game.physics.p2.hitTest(point, this.rockGroup.children));
+                        }, this);
+                        enemy.isChasing = this.tileHits.length === 0 && rocksHit.length === 0;
+                    }
                     if (enemy.isChasing) {
-                        //  TODO
+                        var angle = Math.atan2(this.player.y - enemy.y, this.player.x - enemy.x);
+                        enemy.body.velocity.x = Math.cos(angle) * this.DEMON_CHASE_SPEED;
+                        enemy.body.velocity.y = Math.sin(angle) * this.DEMON_CHASE_SPEED;
                     } else {
                         var compareX = Math.round(enemy.x * 100) / 100;
                         var compareY = Math.round(enemy.y * 100) / 100;
@@ -169,40 +219,39 @@ angular.module('uiApp')
                             (compareY >= enemy.maxY && enemy.body.velocity.y > 0)) {
                             enemy.body.velocity.y *= -1;
                         }
-                        if (Math.abs(enemy.body.velocity.x) < 5) {
-                            enemy.body.velocity.x = Math.sign(enemy.body.velocity.x) * -1 * 25;
+                        if (Math.abs(enemy.body.velocity.x) < this.DEMON_PATROL_SPEED / 5) {
+                            enemy.body.velocity.x = Math.sign(enemy.body.velocity.x) * -1 * this.DEMON_PATROL_SPEED;
                         }
-                        if (Math.abs(enemy.body.velocity.y) < 5) {
-                            enemy.body.velocity.y = Math.sign(enemy.body.velocity.y) * -1 * 25;
+                        if (Math.abs(enemy.body.velocity.y) < this.DEMON_PATROL_SPEED / 5) {
+                            enemy.body.velocity.y = Math.sign(enemy.body.velocity.y) * -1 * this.DEMON_PATROL_SPEED;
                         }
                     }
                 }, this);
                 if (this.cursors.up.isDown) {
-                    this.player.body.moveUp(75);
+                    this.player.body.moveUp(this.PLAYER_MOVE_SPEED);
                 }
                 if (this.cursors.down.isDown) {
-                    this.player.body.moveDown(75);
+                    this.player.body.moveDown(this.PLAYER_MOVE_SPEED);
                 }
                 if (this.cursors.left.isDown) {
-                    this.player.body.moveLeft(75);
+                    this.player.body.moveLeft(this.PLAYER_MOVE_SPEED);
                 }
                 if (this.cursors.right.isDown) {
-                    this.player.body.moveRight(75);
+                    this.player.body.moveRight(this.PLAYER_MOVE_SPEED);
                 }
 
-                this.shadowTexture.context.fillStyle = 'rgb(10, 10, 10)';
+                this.shadowTexture.context.fillStyle = 'rgb(40, 40, 80)';
                 this.shadowTexture.context.fillRect(0, 0, this.game.world.width, this.game.world.height);
-                var radius = 100 + this.game.rnd.integerInRange(1, 10),
+                var radius = this.LIGHT_RADIUS + this.game.rnd.integerInRange(1, 10),
                     heroX = this.player.x,
                     heroY = this.player.y;
 
                 // Draw circle of light
-                var gradient =
-                    this.shadowTexture.context.createRadialGradient(
-                        heroX, heroY, 100 * 0.25,
-                        heroX, heroY, radius);
-                gradient.addColorStop(0, 'rgba(255, 255, 255, 0.6)');
-                gradient.addColorStop(1, 'rgba(255, 255, 255, 0.0)');
+                var gradient = this.shadowTexture.context.createRadialGradient(
+                    heroX, heroY, this.LIGHT_RADIUS * 0.25,
+                    heroX, heroY, radius);
+                gradient.addColorStop(0, 'rgba(200, 200, 200, 0.6)');
+                gradient.addColorStop(1, 'rgba(200, 200, 200, 0.0)');
 
                 this.shadowTexture.context.beginPath();
                 this.shadowTexture.context.fillStyle = gradient;
@@ -214,26 +263,21 @@ angular.module('uiApp')
 
             },
 
-/*
             render: function () {
-                this.game.debug.cameraInfo(game.camera, 0, 0);
-                this.game.debug.spriteInfo(this.player, 400, 0);
-                this.blockLayer.debug = true;
-                this.player.body.debug = true;
-                var count = 0;
-                angular.forEach(this.enemyGroup.children, function (child, index) {
-                    child.body.debug = true;
-                    this.game.debug.spriteInfo(child, index * 350, 100);
-                    ++count;
-                }, this);
-                count = 0;
-                angular.forEach(this.rockGroup.children, function (child, index) {
-                    child.body.debug = true;
-                    this.game.debug.spriteInfo(child, index * 350, 200);
-                    ++count;
-                }, this);
+                if (this.DEBUG) {
+                    this.game.debug.cameraInfo(game.camera, 0, 0);
+                    this.game.debug.spriteInfo(this.player, 400, 0);
+                    this.blockLayer.debug = true;
+                    this.player.body.debug = true;
+                    angular.forEach(this.enemyGroup.children, function (child, index) {
+                        this.game.debug.spriteInfo(child, index * 350, 100);
+                    }, this);
+                    angular.forEach(this.rockGroup.children, function (child, index) {
+                        this.game.debug.spriteInfo(child, index * 350, 200);
+                    }, this);
+                }
             },
-*/
+
             death: function (body) {
                 if (
                     angular.isDefined(body) &&
@@ -241,6 +285,9 @@ angular.module('uiApp')
                     angular.isDefined(body.sprite) &&
                     body.sprite !== null &&
                     body.sprite.key === 'demon') {
+                    //  TODO - dying off screen doesn't reset cleanly without move
+                    this.player.x = 16;
+                    this.player.y = 1264;
                     this.player.kill();
                     this.game.state.start('TestMaze');
                 }
