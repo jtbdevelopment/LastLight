945a88a96fdfbab1163b35fde51d2d2b2443ea65
JTB Development <jtbdevelopment@gmail.com>
2015-12-19 15:04:30 -0500

    death tween

--- ui/app/scripts/controllers/main.js
+++ ui/app/scripts/controllers/main.js
@@ -57,6 +57,8 @@ angular.module('uiApp')
             },
 
             create: function () {
+                this.game.ending = false;
+
                 this.game.physics.startSystem(Phaser.Physics.P2JS);
 
                 var map = this.game.add.tilemap('testmaze');
@@ -167,6 +169,7 @@ angular.module('uiApp')
                 }, this);
 
                 this.player.body.onBeginContact.add(this.death, this);
+
                 this.cursors = this.game.input.keyboard.createCursorKeys();
 
                 this.shadowTexture = this.game.add.bitmapData(this.game.world.width, this.game.world.height);
@@ -179,80 +182,86 @@ angular.module('uiApp')
             update: function () {
                 this.player.body.setZeroVelocity();
 
-                this.enemyGroup.forEach(function (enemy) {
-                    //  TODO - Play sound while chasing or play sound when chase begins?
-                    var ray = new Phaser.Line(enemy.x, enemy.y, this.player.x, this.player.y);
-                    var wasChasing = enemy.isChasing;
-                    enemy.isChasing = false;
-                    if (ray.length < this.DEMON_MAX_SIGHT) {
-                        if (this.DEBUG) {
-                            angular.forEach(this.tileHits, function (tileHit) {
-                                tileHit.debug = false;
-                            });
-                            this.blockLayer.dirty = this.tileHits.length > 0;
-                        }
-                        this.tileHits = this.blockLayer.getRayCastTiles(ray, undefined, true);
-                        if (this.DEBUG) {
-                            angular.forEach(this.tileHits, function (tileHit) {
-                                tileHit.debug = this.DEBUG;
-                            }, this);
-                            this.blockLayer.dirty = this.tileHits.length > 0;
-                        }
+                if (!this.game.ending) {
+                    this.enemyGroup.forEach(function (enemy) {
+                        //  TODO - Play sound while chasing or play sound when chase begins?
+                        var ray = new Phaser.Line(enemy.x, enemy.y, this.player.x, this.player.y);
+                        var wasChasing = enemy.isChasing;
+                        enemy.isChasing = false;
+                        if (ray.length < this.DEMON_MAX_SIGHT) {
+                            if (this.DEBUG) {
+                                angular.forEach(this.tileHits, function (tileHit) {
+                                    tileHit.debug = false;
+                                });
+                                this.blockLayer.dirty = this.tileHits.length > 0;
+                            }
+                            this.tileHits = this.blockLayer.getRayCastTiles(ray, undefined, true);
+                            if (this.DEBUG) {
+                                angular.forEach(this.tileHits, function (tileHit) {
+                                    tileHit.debug = this.DEBUG;
+                                }, this);
+                                this.blockLayer.dirty = this.tileHits.length > 0;
+                            }
 
-                        var rocksHit = [];
-                        var lineCoordinates = ray.coordinatesOnLine(1);
-                        angular.forEach(lineCoordinates, function (point) {
-                            this.game.physics.p2.hitTest({
-                                x: point[0],
-                                y: point[1]
-                            }, this.rockGroup.children, undefined, true).forEach(function (hit) {
-                                rocksHit.push(hit);
-                            });
-                        }, this);
-                        enemy.isChasing = this.tileHits.length === 0 && rocksHit.length === 0;
-                    }
-                    if(!enemy.isChasing && wasChasing ) {
-                        enemy.stopChasingCount++;
-                        if(enemy.stopChasingCount < this.DEMON_STOP_CHASING_AFTER) {
-                            enemy.isChasing = true;
+                            var rocksHit = [];
+                            var lineCoordinates = ray.coordinatesOnLine(1);
+                            angular.forEach(lineCoordinates, function (point) {
+                                this.game.physics.p2.hitTest({
+                                    x: point[0],
+                                    y: point[1]
+                                }, this.rockGroup.children, undefined, true).forEach(function (hit) {
+                                    rocksHit.push(hit);
+                                });
+                            }, this);
+                            enemy.isChasing = this.tileHits.length === 0 && rocksHit.length === 0;
+                        }
+                        if (!enemy.isChasing && wasChasing) {
+                            enemy.stopChasingCount++;
+                            if (enemy.stopChasingCount < this.DEMON_STOP_CHASING_AFTER) {
+                                enemy.isChasing = true;
+                            } else {
+                                enemy.stopChasingCount = 0;
+                            }
+                        }
+                        if (enemy.isChasing) {
+                            var angle = Math.atan2(this.player.y - enemy.y, this.player.x - enemy.x);
+                            enemy.body.velocity.x = Math.cos(angle) * this.DEMON_CHASE_SPEED;
+                            enemy.body.velocity.y = Math.sin(angle) * this.DEMON_CHASE_SPEED;
                         } else {
-                            enemy.stopChasingCount = 0;
+                            var compareX = Math.round(enemy.x * 100) / 100;
+                            var compareY = Math.round(enemy.y * 100) / 100;
+                            if ((compareX <= enemy.minX && enemy.body.velocity.x < 0) ||
+                                (compareX >= enemy.maxX && enemy.body.velocity.x > 0)) {
+                                enemy.body.velocity.x *= -1;
+                            }
+                            if ((compareY <= enemy.minY && enemy.body.velocity.y < 0) ||
+                                (compareY >= enemy.maxY && enemy.body.velocity.y > 0)) {
+                                enemy.body.velocity.y *= -1;
+                            }
+                            if (Math.abs(enemy.body.velocity.x) < this.DEMON_PATROL_SPEED / 5) {
+                                enemy.body.velocity.x = Math.sign(enemy.body.velocity.x) * -1 * this.DEMON_PATROL_SPEED;
+                            }
+                            if (Math.abs(enemy.body.velocity.y) < this.DEMON_PATROL_SPEED / 5) {
+                                enemy.body.velocity.y = Math.sign(enemy.body.velocity.y) * -1 * this.DEMON_PATROL_SPEED;
+                            }
                         }
+                    }, this);
+                    if (this.cursors.up.isDown) {
+                        this.player.body.moveUp(this.PLAYER_MOVE_SPEED);
                     }
-                    if (enemy.isChasing) {
-                        var angle = Math.atan2(this.player.y - enemy.y, this.player.x - enemy.x);
-                        enemy.body.velocity.x = Math.cos(angle) * this.DEMON_CHASE_SPEED;
-                        enemy.body.velocity.y = Math.sin(angle) * this.DEMON_CHASE_SPEED;
-                    } else {
-                        var compareX = Math.round(enemy.x * 100) / 100;
-                        var compareY = Math.round(enemy.y * 100) / 100;
-                        if ((compareX <= enemy.minX && enemy.body.velocity.x < 0) ||
-                            (compareX >= enemy.maxX && enemy.body.velocity.x > 0)) {
-                            enemy.body.velocity.x *= -1;
-                        }
-                        if ((compareY <= enemy.minY && enemy.body.velocity.y < 0) ||
-                            (compareY >= enemy.maxY && enemy.body.velocity.y > 0)) {
-                            enemy.body.velocity.y *= -1;
-                        }
-                        if (Math.abs(enemy.body.velocity.x) < this.DEMON_PATROL_SPEED / 5) {
-                            enemy.body.velocity.x = Math.sign(enemy.body.velocity.x) * -1 * this.DEMON_PATROL_SPEED;
-                        }
-                        if (Math.abs(enemy.body.velocity.y) < this.DEMON_PATROL_SPEED / 5) {
-                            enemy.body.velocity.y = Math.sign(enemy.body.velocity.y) * -1 * this.DEMON_PATROL_SPEED;
-                        }
+                    if (this.cursors.down.isDown) {
+                        this.player.body.moveDown(this.PLAYER_MOVE_SPEED);
                     }
-                }, this);
-                if (this.cursors.up.isDown) {
-                    this.player.body.moveUp(this.PLAYER_MOVE_SPEED);
-                }
-                if (this.cursors.down.isDown) {
-                    this.player.body.moveDown(this.PLAYER_MOVE_SPEED);
-                }
-                if (this.cursors.left.isDown) {
-                    this.player.body.moveLeft(this.PLAYER_MOVE_SPEED);
-                }
-                if (this.cursors.right.isDown) {
-                    this.player.body.moveRight(this.PLAYER_MOVE_SPEED);
+                    if (this.cursors.left.isDown) {
+                        this.player.body.moveLeft(this.PLAYER_MOVE_SPEED);
+                    }
+                    if (this.cursors.right.isDown) {
+                        this.player.body.moveRight(this.PLAYER_MOVE_SPEED);
+                    }
+                } else {
+                    this.enemyGroup.forEach(function (enemy) {
+                        enemy.body.setZeroVelocity()
+                    });
                 }
 
                 this.shadowTexture.context.fillStyle = 'rgb(40, 40, 80)';
@@ -300,11 +309,18 @@ angular.module('uiApp')
                     angular.isDefined(body.sprite) &&
                     body.sprite !== null &&
                     body.sprite.key === 'demon') {
+                    this.game.ending = true;
                     //  TODO - dying off screen doesn't reset cleanly without move
-                    this.player.x = 16;
-                    this.player.y = 1264;
-                    this.player.kill();
-                    this.game.state.start('TestMaze');
+                    var savedRadius = this.LIGHT_RADIUS;
+                    var deathTween = this.game.add.tween(this);
+                    deathTween.to({LIGHT_RADIUS: 0}, 1000, Phaser.Easing.Power1, true);
+                    deathTween.onComplete.add(function () {
+                        this.player.x = 16;
+                        this.player.y = 1264;
+                        this.player.kill();
+                        this.LIGHT_RADIUS = savedRadius;
+                        this.game.state.start('TestMaze');
+                    }, this);
                 }
             }
         };
@@ -314,47 +330,3 @@ angular.module('uiApp')
         game.state.start('TitleScreen');
     });
 
-/*  This is phaser accelerate to target code  */
-/*
- var game = new Phaser.Game(800, 600, Phaser.AUTO, 'phaser-example', { preload: preload, create: create, update:update });
-
- function preload() {
- game.load.image('car', 'assets/sprites/car.png');
- game.load.image('tinycar', 'assets/sprites/tinycar.png');
- }
-
- function create() {
- game.physics.startSystem(Phaser.Physics.P2JS);
- bullets = game.add.group();
- for (var i = 0; i < 10; i++) {
- var bullet = bullets.create(game.rnd.integerInRange(200, 1700), game.rnd.integerInRange(-200, 400), 'tinycar');
- game.physics.p2.enable(bullet,false);
- }
- cursors = game.input.keyboard.createCursorKeys();
- ship = game.add.sprite(32, game.world.height - 150, 'car');
- game.physics.p2.enable(ship);
- };
-
- function update() {
- bullets.forEachAlive(moveBullets,this);  //make bullets accelerate to ship
-
- if (cursors.left.isDown) {ship.body.rotateLeft(100);}   //ship movement
- else if (cursors.right.isDown){ship.body.rotateRight(100);}
- else {ship.body.setZeroRotation();}
- if (cursors.up.isDown){ship.body.thrust(400);}
- else if (cursors.down.isDown){ship.body.reverse(400);}
- };
-
-
- function moveBullets (bullet) {
- accelerateToObject(bullet,ship,30);  //start accelerateToObject on every bullet
- }
-
- function accelerateToObject(obj1, obj2, speed) {
- if (typeof speed === 'undefined') { speed = 60; }
- var angle = Math.atan2(obj2.y - obj1.y, obj2.x - obj1.x);
- obj1.body.rotation = angle + game.math.degToRad(90);  // correct angle of angry bullets (depends on the sprite used)
- obj1.body.force.x = Math.cos(angle) * speed;    // accelerateToObject
- obj1.body.force.y = Math.sin(angle) * speed;
- }
- */
