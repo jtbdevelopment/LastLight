7c264478c2e732008577b46d2b954c3f1cc407f5
JTB Development <jtbdevelopment@gmail.com>
2015-12-28 10:43:16 -0500

    reformat

--- ui/app/scripts/services/GameFactory.js
+++ ui/app/scripts/services/GameFactory.js
@@ -2,13 +2,14 @@
 'use strict';
 
 angular.module('uiApp').factory('GameFactory',
-    ['TitleScreenState', 'Act1MazeState', function (TitleScreenState, Act1MazeState) {
-        var game = new Phaser.Game(800, 400, Phaser.AUTO, 'phaser');
+    ['TitleScreenState', 'Act1MazeState',
+        function (TitleScreenState, Act1MazeState) {
+            var game = new Phaser.Game(800, 400, Phaser.AUTO, 'phaser');
 
-        game.state.add('TitleScreen', TitleScreenState);
-        game.state.add('Act1', Act1MazeState);
+            game.state.add('TitleScreen', TitleScreenState);
+            game.state.add('Act1', Act1MazeState);
 
-        return game;
-    }
+            return game;
+        }
     ]
 );
